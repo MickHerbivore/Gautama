bd170f29c55010703666bcc5c301a7fdbfc3f2f2
Miguel Muñoz Caro <mick.munozcaro@gmail.com>
2022-04-01 20:08:00 -0300

    (build) Intento 3.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,8 +25,8 @@ registerLocaleData(localeCl, 'cl')
     AppRoutingModule,
     AuthModule.forRoot({
       domain: 'dev-w7nn53is.us.auth0.com',
-      clientId: 'vxElN6FhcyH3yVESviojk6diYmR2fi4J',
-      // clientId: '3YFoQZdxaYs3Cj3QCZwEsdnYCCfyiQMp',
+      // clientId: 'vxElN6FhcyH3yVESviojk6diYmR2fi4J',
+      clientId: '3YFoQZdxaYs3Cj3QCZwEsdnYCCfyiQMp',
       redirectUri: window.location.origin
     }),
     HttpClientModule,
